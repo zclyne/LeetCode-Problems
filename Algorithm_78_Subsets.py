14392569e8719b30e1537940b453ab6c35186e48
Yifan Zhang <zyfinori@gmail.com>
2018-03-18 15:05:02 +0800

    commit

--- Algorithm_78_Subsets.py
+++ Algorithm_78_Subsets.py
@@ -1,21 +1,16 @@
 class Solution:
-    def get(self, nums, bits, res, cur):
-        if cur==len(bits):
-            tmpRes=[]
-            for i in range(len(bits)):
-                if bits[i]==1:
-                    tmpRes.append(nums[i])
-            res.append(tmpRes)
+    def get(self, nums, tmpRes, res, cur):
+        if cur==len(nums):
+            res.append(tmpRes[:])
             return
-        bits[cur]=1
-        Solution.get(self,nums,bits,res,cur+1)
-        bits[cur]=0
-        Solution.get(self,nums,bits,res,cur+1)
+        tmpRes.append(nums[cur])
+        Solution.get(self,nums,tmpRes,res,cur+1)
+        tmpRes.pop()
+        Solution.get(self,nums,tmpRes,res,cur+1)
 
     def subsets(self, nums):
         n=len(nums)
         if n==0: return [[]]
         res=[]
-        bits=[0 for i in range(n)]
-        Solution.get(self,nums,bits,res,0)
+        Solution.get(self,nums,[],res,0)
         return res
