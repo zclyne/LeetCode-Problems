305a96993f67931077f1b929898b651caec72b4b
Yifan Zhang <zyfinori@gmail.com>
2018-05-03 20:45:09 +0800

    commit

--- Algorithm_92_Reverse Linked List II.cpp
+++ Algorithm_92_Reverse Linked List II.cpp
@@ -7,8 +7,30 @@ struct ListNode {
 };
 class Solution {
 public:
-    
     ListNode* reverseBetween(ListNode* head, int m, int n) {
-        
+        if (!head->next) return head;
+        int cnt=0;
+        ListNode *newhead, *newtail, *tmp, *node_before_m, *node_after_n=NULL, *newnode;
+        ListNode *tmphead=new ListNode(0);
+        tmphead->next=head;
+        for (tmp=tmphead;tmp!=NULL;tmp=tmp->next)
+        {
+            if (cnt==m-1) node_before_m=tmp;
+            else if (cnt==n+1) node_after_n=tmp;
+            else if (cnt>=m && cnt<=n)
+            {
+                if (cnt==m) newhead=newtail=new ListNode(tmp->val);
+                else
+                {
+                    newnode=new ListNode(tmp->val);
+                    newnode->next=newhead;
+                    newhead=newnode;
+                }
+            }
+            cnt++;
+        }
+        node_before_m->next=newhead;
+        newtail->next=node_after_n;
+        return tmphead->next;
     }
 };
---
+++ Algorithm_93_Restore IP Addresses.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<string> restoreIpAddresses(string s) {
+        
+    }
+};
