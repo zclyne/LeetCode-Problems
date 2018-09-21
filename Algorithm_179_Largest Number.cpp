3dd9d511017e635179c871f80b6a4669d8ee026b
Yifan_MacMini <zyfinori@gmail.com>
2018-09-21 21:32:32 +0800

    Complete Algorithm_179

--- Algorithm_179_Largest Number.cpp
+++ Algorithm_179_Largest Number.cpp
@@ -8,35 +8,18 @@ class Solution
   public:
     string largestNumber(vector<int> &nums)
     {
-        string res = to_string(nums[0]);
-        if (nums.size() == 1) return res;
-        for (int i = 1; i < nums.size() ; i++)
-        {
-            string tmp = to_string(nums[i]);
-            res = link(res, tmp);
-        }
-        return res;
+        if (nums.size() == 0) return "";
+        vector<string> numsStr;
+        string res;
+        for (int i = 0; i < nums.size() ; i++) numsStr.push_back(to_string(nums[i]));
+        sort(numsStr.begin(), numsStr.end(), myCompare);
+        for (int i = 0; i < numsStr.size() ; i++) res += numsStr[i];
+        int start;
+        for (start = 0; res[start] == '0' ; start++);
+        res = res.substr(start, res.size() - start);
+        return res.size() > 0 ? res : "0";
     }
-    string link(string &s1, string &s2)
-    {
-        int len1 = s1.size(), len2 = s2.size();
-        int minLen = min(len1, len2);
-        for (int i = 0; i < minLen; i++)
-        {
-            if ((s1[i] - '0') < (s2[i] - '0')) return s2 + s1;
-            else if ((s1[i] - '0') > (s2[i] - '0')) return s1 + s2;
-        }
-        if (len1 == len2) return s1 + s2;
-        if (minLen == len1)
-        {
-            string subs2 = s2.substr(minLen, s2.size() - minLen);
-            return s1 + link(s1, subs2);
-        }
-        else
-        {
-            string subs1 = s1.substr(minLen, s1.size() - minLen);
-            return s2 + link(subs1, s2);
-        }
-        
-    }
+    struct compare {
+        bool operator() (string &s1, string &s2) {return s1 + s2 > s2 + s1;}
+    } myCompare;
 };
