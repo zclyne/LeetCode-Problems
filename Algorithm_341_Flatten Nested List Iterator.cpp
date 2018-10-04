2c1a92809efc296e8a769ea3a026c189f594cdca
Yifan Zhang <zyfinori@gmail.com>
2018-10-05 00:35:07 +0800

    Complete Algorithm_341/343

--- Algorithm_341_Flatten Nested List Iterator.cpp
+++ Algorithm_341_Flatten Nested List Iterator.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 using namespace std;
 
 // This is the interface that allows for creating nested lists.
@@ -18,17 +19,29 @@ class NestedInteger {
     const vector<NestedInteger> &getList() const;
 };
 class NestedIterator {
+private:
+    stack<NestedInteger> nestedStack;
+    void buildStack(NestedInteger curNest)
+    {
+        if (curNest.isInteger()) nestedStack.push(curNest);
+        else
+        {
+            vector<NestedInteger> curVec = curNest.getList();
+            for (int i = curVec.size() - 1; i >= 0; i--)
+            buildStack(curVec[i]);
+        }
+    }
 public:
     NestedIterator(vector<NestedInteger> &nestedList) {
-        
+        for (int i = nestedList.size() - 1; i >= 0; i--) buildStack(nestedList[i]);
     }
-
     int next() {
-        
+        int res = nestedStack.top().getInteger();
+        nestedStack.pop();
+        return res;
     }
-
     bool hasNext() {
-        
+        return !nestedStack.empty();
     }
 };
 
@@ -37,3 +50,33 @@ public:
  * NestedIterator i(nestedList);
  * while (i.hasNext()) cout << i.next();
  */
+
+// better java solution without recursion
+// public class NestedIterator implements Iterator<Integer> {
+//     Stack<NestedInteger> stack = new Stack<>();
+//     public NestedIterator(List<NestedInteger> nestedList) {
+//         for(int i = nestedList.size() - 1; i >= 0; i--) {
+//             stack.push(nestedList.get(i));
+//         }
+//     }
+
+//     @Override
+//     public Integer next() {
+//         return stack.pop().getInteger();
+//     }
+
+//     @Override
+//     public boolean hasNext() {
+//         while(!stack.isEmpty()) {
+//             NestedInteger curr = stack.peek();
+//             if(curr.isInteger()) {
+//                 return true;
+//             }
+//             stack.pop();
+//             for(int i = curr.getList().size() - 1; i >= 0; i--) {
+//                 stack.push(curr.getList().get(i));
+//             }
+//         }
+//         return false;
+//     }
+// }
