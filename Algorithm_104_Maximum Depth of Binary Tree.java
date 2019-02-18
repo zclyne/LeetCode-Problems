b5246f12e23c87c935e7e460bcaf15f86e138663
zclyne <zyfinori@gmail.com>
2019-02-18 16:38:43 +0800

    Add a BFS Solution to Algorithm_104

--- Algorithm_104_Maximum Depth of Binary Tree.java
+++ Algorithm_104_Maximum Depth of Binary Tree.java
@@ -14,3 +14,30 @@ class Solution {
         return root == null ? 0 : 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
     }
 }
+
+// BFS Solution
+// int maxDepth(TreeNode *root)
+// {
+//     if(root == NULL)
+//         return 0;
+    
+//     int res = 0;
+//     queue<TreeNode *> q;
+//     q.push(root);
+//     while(!q.empty())
+//     {
+//         ++ res;
+//         for(int i = 0, n = q.size(); i < n; ++ i)
+//         {
+//             TreeNode *p = q.front();
+//             q.pop();
+            
+//             if(p -> left != NULL)
+//                 q.push(p -> left);
+//             if(p -> right != NULL)
+//                 q.push(p -> right);
+//         }
+//     }
+    
+//     return res;
+// }
