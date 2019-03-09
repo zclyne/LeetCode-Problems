1a19f9b9f26de98be8e003d18a34c0bc2ba4f4d0
zclyne <zyfinori@gmail.com>
2019-03-09 22:40:25 +0800

    Modified Algorithm_19

--- Algorithm_19_Remove Nth Node From End of List.java
+++ Algorithm_19_Remove Nth Node From End of List.java
@@ -4,14 +4,12 @@
 // 注意初始时设置slow.next = head是为了考虑[1, 2] n = 2这种情况
 // 此时，要删除的节点恰好是head
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode slow = new ListNode(-1), newHead = slow, fast = head;
