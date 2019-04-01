6763ddba70d0f0ae85ba6421a990736a38cb79fd
zclyne <zyfinori@gmail.com>
2019-04-02 00:14:28 +0800

    Modify Algorithm_21

--- Algorithm_21_Merge Two Sorted Lists.java
+++ Algorithm_21_Merge Two Sorted Lists.java
@@ -1,13 +1,12 @@
 // 思路：用2个指针遍历2个链表，根据两指针对应值的大小，取较小的作为新的节点，并把指针向后挪一位
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode cur = new ListNode(0);
