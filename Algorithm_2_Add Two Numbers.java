4b8238c11bb36d1e3549bd4fbdc94e56721ef4a9
Yifan - Macbook Pro <zyfinori@outlook.com>
2019-12-29 23:53:31 +0800

    Optimized Algorithm_2

--- Algorithm_2_Add Two Numbers.java
+++ Algorithm_2_Add Two Numbers.java
@@ -8,29 +8,25 @@ class ListNode {
 
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int val1 = l1.val, val2 = l2.val;
-        int sum = (val1 + val2) % 10, carry = (val1 + val2) / 10;
-        ListNode start = new ListNode(sum);
-        ListNode cur = start;
-        l1 = l1.next;
-        l2 = l2.next;
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        }
+        int carry = 0;
+        ListNode head = new ListNode(0), current = head;
         while (l1 != null || l2 != null) {
-            val1 = l1 == null ? 0 : l1.val;
-            val2 = l2 == null ? 0 : l2.val;
-            sum = (val1 + val2 + carry) % 10;
+            int val1 = l1 == null ? 0 : l1.val;
+            int val2 = l2 == null ? 0 : l2.val;
+            current.next = new ListNode((val1 + val2 + carry) % 10);
             carry = (val1 + val2 + carry) / 10;
-            cur.next = new ListNode(sum);
-            cur = cur.next;
-            if (l1 != null) {
-                l1 = l1.next;
-            }
-            if (l2 != null) {
-                l2 = l2.next;
-            }
+            current = current.next;
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
         }
-        if (carry == 1) {
-            cur.next = new ListNode(1);
+        if (carry != 0) {
+            current.next = new ListNode(carry);
         }
-        return start;
+        return head.next;
     }
 }
