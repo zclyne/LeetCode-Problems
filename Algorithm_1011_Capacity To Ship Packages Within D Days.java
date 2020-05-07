f35d3e3d83f6f165f5da704cd3aa689dceeed545
Yifan-NUC <zyfinori@gmail.com>
2020-05-08 00:08:50 +0800

    Complete Algorithm_911/1011

--- Algorithm_1011_Capacity To Ship Packages Within D Days.java
+++ Algorithm_1011_Capacity To Ship Packages Within D Days.java
@@ -1,5 +1,36 @@
+import java.util.Arrays;
+
+// 思路：二分查找。初始的左侧边界值为weights中最大的那个，右侧边界值为
+// 所有weight之和。设当前迭代的maxWeight为left和right的中位数
+// 然后遍历weights，daysNeed是当前maxWeight所需要的运完所有货物所需的天数
+// curWeight是当天所运的货物总重量。
+// 若当前maxWeight太小，遍历完成后，daysNeed会大于D，这时应把left收缩到maxWeight + 1
+// 若当前maxWeight能满足要求，则right = maxWeight，但不是maxWeight - 1，因为当前maxWeight
+// 有可能是最终的答案
+
 class Solution {
     public int shipWithinDays(int[] weights, int D) {
-        
+        int left = 0, right = 0;
+        for (int weight : weights) {
+            left = Math.max(left, weight);
+            right += weight;
+        }
+        while (left < right) {
+            int maxWeight = left + (right - left) / 2;
+            int daysNeed = 1, curWeight = 0;
+            for (int weight : weights) {
+                curWeight += weight;
+                if (curWeight > maxWeight) {
+                    daysNeed++;
+                    curWeight = weight;
+                }
+            }
+            if (daysNeed > D) { // maxWeight is too small
+                left = maxWeight + 1;
+            } else { // 
+                right = maxWeight;
+            }
+        }
+        return left;
     }
 }
--- Algorithm_911_Online Election.java
+++ Algorithm_911_Online Election.java
@@ -1,12 +1,48 @@
+// 思路：数组leads中的leads[i]记录在times[i]时刻票数最多的person
+// 在初始化阶段，遍历一遍persons并填充数组leads
+// q()中，先用二分查找的方法找到idx，使得times[idx] <= t
+// leads[idx]就是t时刻票数最多的person
+
 class TopVotedCandidate {
 
+    private int[] leads;
+    private int[] times;
+
     public TopVotedCandidate(int[] persons, int[] times) {
-        
+        int[] voteCounts = new int[persons.length];
+        leads = new int[persons.length];
+        this.times = times;
+        int curLead = -1, curMaxVote = 0;
+        for (int i = 0; i < persons.length; i++) {
+            int person = persons[i];
+            // notice >= because if there is a tie, the most recent vote wins
+            if (++voteCounts[person] >= curMaxVote) {
+                curLead = person;
+                curMaxVote = voteCounts[person];
+            }
+            leads[i] = curLead;
+        }
     }
     
     public int q(int t) {
-        
+        int idx = binarySearch(t);
+        return leads[idx];
     }
+
+    // return the biggest idx in times that times[idx] <= t
+    private int binarySearch(int t) {
+        int left = 0, right = times.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (times[mid] > t) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return right;
+    }
+
 }
 
 /**
