a256a3417199e88b8f6e68be8327d8ee2165d46e
Yifan-NUC <zyfinori@gmail.com>
2020-07-16 18:01:41 +0800

    Complete ALgorithm_135/137/140

--- Algorithm_135_Candy.java
+++ Algorithm_135_Candy.java
@@ -1,69 +1,29 @@
-// 思路：
+// 思路：先正序遍历，确保若右边的child的rating比他相邻的左边的child的rating要高，则右边的child拥有更多candy
+// 再逆序遍历，确保若左边的child的rating比他相邻的右边的child的rating要高，则左边的child拥有更多的candy
+// 左后遍历一遍candies，把每个child拥有的candy数求和，就是结果
 
 class Solution {
     public int candy(int[] ratings) {
-        int continuousMonotonicCount = 0, result = 0;
-        // 1 for increasing, -1 for decreasing, 0 for not change
-        // for the initial situation, prevDirection is 0
-        int prevDirection = 0;
-        int lastContinuousIncreasingCount = 0;
-        boolean flag = false;
-        for (int i = 0; i < ratings.length; i++) {
-            int nextDirection = getDirection(ratings, i);
-            if (prevDirection == 1) {
-                continuousMonotonicCount++;
-                result += continuousMonotonicCount;
-                if (nextDirection == 1) {
-                    // do nothing
-                } else if (nextDirection == 0) {
-                    continuousMonotonicCount = 1;
-                } else { // this is the tricky part
-                    lastContinuousIncreasingCount = continuousMonotonicCount;
-                    flag = true;
-                    continuousMonotonicCount = 1;
-                }
-            } else if (prevDirection == 0) {
-                continuousMonotonicCount = 1;
-                result += continuousMonotonicCount;
-            } else {
-                continuousMonotonicCount++;
-                result += continuousMonotonicCount;
-                if (nextDirection == 1) {
-                    if (flag) {
-                        if (lastContinuousIncreasingCount > continuousMonotonicCount) {
-                            result += lastContinuousIncreasingCount - continuousMonotonicCount;
-                        }
-                        flag = false;
-                    }
-                    continuousMonotonicCount = 1;
-                } else if (nextDirection == 0) {
-                    if (flag) {
-                        if (lastContinuousIncreasingCount > continuousMonotonicCount) {
-                            result += lastContinuousIncreasingCount - continuousMonotonicCount;
-                        }
-                        flag = false;
-                    }
-                    continuousMonotonicCount = 1;
-                } else {
-                    // do nothing
-                }
+        int result = 0;
+        int[] candies = new int[ratings.length];
+        Arrays.fill(candies, 1);
+        // every child with a higher rating than the child on his or her left
+        // should get more candies
+        for (int i = 1; i < ratings.length; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
             }
-            prevDirection = nextDirection;
+        }
+        // every child with a higher rating than the child on his or her right
+        // should get more candies
+        for (int i = ratings.length - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+        for (int candy : candies) {
+            result += candy;
         }
         return result;
-    }
-
-    private int getDirection(int[] ratings, int i) {
-        if (i == ratings.length - 1) { // last element
-            return 0;
-        } else {
-            if (ratings[i + 1] > ratings[i]) {
-                return 1;
-            } else if (ratings[i + 1] == ratings[i]) {
-                return 0;
-            } else {
-                return -1;
-            }
-        }
     }
 }
