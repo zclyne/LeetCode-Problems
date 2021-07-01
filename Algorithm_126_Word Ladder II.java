ebf6d0ade01b459494114da0aae0767aa014c5a5
Yifan-NUC <zyfinori@gmail.com>
2021-07-01 20:39:24 +0800

    finished algorithm_126

--- Algorithm_126_Word Ladder II.java
+++ Algorithm_126_Word Ladder II.java
@@ -6,6 +6,58 @@ import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
 
+// 二刷BFS解法
+// 队列中保存的不是单词本身，而是整个路径上的所有单词，通过逗号分隔
+// 这样，BFS路径上的所有单词都被自动保存下来了
+
+class Solution {
+    public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+        Set<String> dict = new HashSet<>(wordList);
+        List<List<String>> result = new ArrayList<>();
+
+        if (!dict.contains(endWord)) {
+            return result;
+        }
+
+        Set<String> visited = new HashSet<>();
+        Queue<String> queue = new LinkedList<>();
+
+        queue.offer(beginWord);
+        int shortestPathLength = 0;
+
+        while (!queue.isEmpty()) {
+            String str = queue.poll();
+            String[] words = str.split(",");
+            String curWord = words[words.length - 1];
+            visited.add(curWord);
+
+            // change each letter and see whether the new word exists in dict
+            for (int i = 0; i < curWord.length(); i++) {
+                for (int j = 0; j < 26; j++) {
+                    char c = (char) ('a' + j);
+                    String newWord = curWord.substring(0, i) + c + curWord.substring(i + 1);
+                    if (newWord.equals(endWord)) { // found an valid path to the endWord
+                        if (shortestPathLength == 0 || words.length + 1 == shortestPathLength) { // path not found before, this is the shortest path
+                            List<String> path = new ArrayList<>();
+                            path.addAll(Arrays.asList(words));
+                            path.add(endWord);
+                            result.add(path);
+                            shortestPathLength = path.size();
+                        } else { // not the shortest path, no need to continue the BFS
+                            return result;
+                        }
+                    } else if (dict.contains(newWord) && !visited.contains(newWord)) { // newWord exists in dict and is not visited before
+                        String newStr = str + ',' + newWord; // append the newWord to the string so that the path is automatically saved
+                        queue.offer(newStr);
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
+
 // BFS + DFS Solution
 // 思路：先用BFS来保存从beginWord到wordList中的各个单词的距离，记录在distance中
 // 并在BFS的过程中，计算wordList中的每个单词的在wordList中的相邻单词，记录在nodeNeighbors中
